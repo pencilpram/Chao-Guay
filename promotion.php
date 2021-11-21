192498b7096bfb84ee115ec9b76899505b3b8c5a
pencilpram <pencilpram@gmail.com>
2021-11-21 16:50:14 +0700

    edit style

--- promotion.php
+++ promotion.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html>
 
@@ -15,39 +12,23 @@ session_start();
 </head>
 
 <body class="bg-dark" style="font-family: Rationale; text-align:center;">
-
     <div class="py-5">
-        <div class="row" >
-                <div class="col-md-12"><a class="btn btn-primary" href="home.php"
-                    style="background-color:#161719; border:1px solid black; color:#161719;">
-                    <img
-                    src="images/return.png" style="width:40px; height:40px;"></a>
-                </div>
-                <div class="text-center mx-auto col-md-12">
-                            
-                </div>
-            </div>
-        <div class="container" style="margin-top: 20%; margin-left:10%; margin-left:16%;">
-            <div class="row">
-                <div class="col-md-2"><a class="btn btn-primary" style="height:100px; width:100px; background-color:#EDB63C; background-image:'images/ticket.png';border:1px solid black; color:#161719; position:relative; padding-top:24%;" href="promotion_insert.php">Insert</a>
-                </div>
-                <div class="col-md-2"><a class="btn btn-primary" style="height:100px; width:100px; background-color:#EDB63C; border:1px solid black; color:#161719; position:relative; padding-top:24%;" href="promotion_delete.php">Delete</a>
-                </div>
-                
+        <div class="container">
+            <div class="row" style="text-align: left;">
+                <div class="col-md-12"><a class="btn btn-primary" href="home.php" style="background-color:#161719; border:1px solid black; color:#161719;"><img src="images/return.png" style="width:40px; height:40px;"></a></div>
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="">
-        <div class="container"> <a class="navbar-brand" href="#">
-            </a>
-            <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar11">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbar11">
+    <div class="py-5">
+        <div class="container">
+            <div class="row" style="margin-top:8%;">
+                <div class="col-md-6"><a class="btn btn-primary" style="height:100px; width:100px; background-color:#EDB63C; border:1px solid black; color:#161719; position:relative; padding-top:11%;" href="insert.php">INSERT</a>
+                </div>
+                <div class="col-md-6"><a class="btn btn-primary" style="height:100px; width:100px; background-color:#EDB63C; border:1px solid black; color:#161719; position:relative; padding-top:11%;" href="movielist_delete.php">DELETE</a>
+                </div>
             </div>
         </div>
-        
-    </nav>
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
